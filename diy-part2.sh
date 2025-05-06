b3d28e370104ac457bf811ab907a342a23c4ecf7
BigBB2110 <50496854+jh0072110@users.noreply.github.com>
2025-05-06 22:08:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -28,7 +28,10 @@ rm -rf feeds/packages/net/geoview
 rm -rf feeds/packages/lang/golang
 sed -i 's/192.168.1.1/172.17.17.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
-git clone -b openwrt-24.10 https://github.com/sbwml/luci-theme-argon package/argonx
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config package/argon-config
+git clone -b js https://github.com/sirpdboy/luci-theme-kucat package/kucat
+git clone https://github.com/sirpdboy/luci-app-advancedplus package/kuplus
 git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
 git clone -b main https://github.com/xiaorouji/openwrt-passwall package/psw
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/pswcore
