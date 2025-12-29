33b52ff27d29136f268a7b117282f00c9eccb337
BigBB2110 <50496854+jh0072110@users.noreply.github.com>
2025-12-29 10:20:30 +0800

    Remove and add packages in diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,6 +25,7 @@ rm -rf feeds/packages/net/ddns-go
 rm -rf feeds/packages/net/xray-core
 rm -rf feeds/packages/net/sing-box
 rm -rf feeds/packages/net/geoview
+rm -rf feeds/packages/net/tcping
 rm -rf feeds/packages/lang/golang
 sed -i 's/192.168.1.1/172.17.17.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
@@ -34,5 +35,4 @@ git clone https://github.com/sbwml/luci-app-openlist2 package/luci-app-olist
 git clone -b main https://github.com/xiaorouji/openwrt-passwall package/psw
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/pswcore
 git clone https://github.com/sirpdboy/luci-app-ddns-go package/ddns-go
-git clone https://github.com/sbwml/luci-app-mosdns package/mosdns
 git clone -b dev https://github.com/immortalwrt/homeproxy package/homeproxy
