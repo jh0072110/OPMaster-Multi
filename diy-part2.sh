6b78163501930f23790bb2b0af67ee2ab7fb5a6c
BigBB2110 <50496854+jh0072110@users.noreply.github.com>
2026-01-05 12:05:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -31,8 +31,5 @@ sed -i 's/192.168.1.1/172.17.17.1/g' package/base-files/files/bin/config_generat
 git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
 git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/argon-config
-git clone https://github.com/sbwml/luci-app-openlist2 package/luci-app-olist
 git clone -b main https://github.com/xiaorouji/openwrt-passwall package/psw
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/pswcore
-git clone https://github.com/sirpdboy/luci-app-ddns-go package/ddns-go
-git clone -b dev https://github.com/immortalwrt/homeproxy package/homeproxy
