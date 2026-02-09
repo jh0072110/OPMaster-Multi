ec3a54a6ff7a485997d518735036cc6e14e3040a
BigBB2110 <50496854+jh0072110@users.noreply.github.com>
2026-02-09 11:03:58 +0800

    Change default IP and clean up package clones
    Updated default IP address in config_generate script and removed unnecessary package clones.

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,12 +27,10 @@ rm -rf feeds/packages/net/sing-box
 rm -rf feeds/packages/net/geoview
 rm -rf feeds/packages/net/tcping
 rm -rf feeds/packages/lang/golang
-sed -i 's/192.168.1.1/172.17.17.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/argon-config
 git clone https://github.com/sbwml/luci-app-openlist2 package/luci-app-olist
 git clone -b main https://github.com/Openwrt-Passwall/openwrt-passwall package/psw
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/pswcore
-git clone https://github.com/sirpdboy/luci-app-ddns-go package/ddns-go
-git clone -b dev https://github.com/immortalwrt/homeproxy package/homeproxy
