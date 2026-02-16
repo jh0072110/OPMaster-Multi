e92c35080307b97e0fb7b764d60ebd5725cab63d
BigBB2110 <50496854+jh0072110@users.noreply.github.com>
2026-02-16 22:56:38 +0800

    Add bandix package to diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -37,3 +37,4 @@ git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/
 git clone -b dev https://github.com/immortalwrt/homeproxy package/homeproxy
 git clone https://github.com/sirpdboy/luci-app-ddns-go package/ddns-go
 git clone https://github.com/sbwml/luci-app-mosdns package/mosdns
+git clone https://github.com/timsaya/luci-app-bandix package/bandix
